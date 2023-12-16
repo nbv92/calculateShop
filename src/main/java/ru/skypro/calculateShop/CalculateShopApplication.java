4bc4623ddfe775af0e4afefaa9b6075381d20b9f
Timofey <timofey.sharipov@gmail.com>
2023-12-16 05:50:48 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/calculateShop/CalculateShopApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.calculateShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/calculateShop/CalculateShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.calculateShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
